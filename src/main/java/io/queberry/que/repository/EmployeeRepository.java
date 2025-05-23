bc77e7e041e2362345cd0e27e34131561866085c
savan-sudhakaran <savan.sudhakaran@queberry.com>
2025-05-23 13:01:43 +0530

    Changes

--- src/main/java/io/queberry/que/repository/EmployeeRepository.java
+++ src/main/java/io/queberry/que/repository/EmployeeRepository.java
@@ -2,38 +2,52 @@ package io.queberry.que.repository;
 
 import io.queberry.que.entity.Branch;
 import io.queberry.que.entity.Employee;
-import jakarta.persistence.QueryHint;
+import io.queberry.que.entity.Role;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.QueryHints;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, String> {
 
-    @QueryHints(value = {
-            @QueryHint(name = "org.hibernate.cacheable", value = "true"),
-            @QueryHint(name = "org.hibernate.cacheRegion", value = "EmployeeUserCache")
-    })
-    @Query("SELECT e FROM que_employee e WHERE e.username = :username")
-    Employee findByUsername(@Param("username") String username);
+//    @QueryHints(value = {
+//            @QueryHint(name = "org.hibernate.cacheable", value = "true"),
+//            @QueryHint(name = "org.hibernate.cacheRegion", value = "EmployeeUserCache")
+//    })
+//    @Query("SELECT e FROM que_employee e WHERE e.username = :username")
+//    Employee findByUsername(@Param("username") String username);
+//
+////    Set<Employee> findByBranch(Set<String> branches);
+//
+//
+//    Set<Employee> findByBranchIn(Set<String> branches);
+//
+//    Optional<Employee> findByLoggedCounter(String cid);
+//
+//    @Query(value = "SELECT e FROM que_employee e WHERE e.id = :id")
+//    Optional<Employee> findEmployeeById(@Param("id") String id);
+//
+//    @Query(value = "SELECT e FROM que_employee e WHERE e.username = :username")
+//    Optional<Employee> findEmployeeByUsername(@Param("username") String username);
+Employee findByUsername(String username);
 
-//    Set<Employee> findByBranch(Set<String> branches);
+    Page<Employee> findByUsernameContainingIgnoreCase(String username, Pageable pageable);
 
+    int countAllByActiveTrue();
 
-    Set<Employee> findByBranchIn(Set<String> branches);
+    List<Employee> findByBranchesIn(Set<String> branches);
 
-    Optional<Employee> findByLoggedCounter(String cid);
+    List<Employee> findAllByUsername(String username);
 
-    @Query(value = "SELECT e FROM que_employee e WHERE e.id = :id")
-    Optional<Employee> findEmployeeById(@Param("id") String id);
+    Optional<Employee> findById(String id);
 
-    @Query(value = "SELECT e FROM que_employee e WHERE e.username = :username")
-    Optional<Employee> findEmployeeByUsername(@Param("username") String username);
+    List<Employee> findByActiveTrueAndBranchesAndRolesContaining(Branch branch, Role role);
+
 
 }
 
