362cf43b2003cfdc163c599908853e9afe8d09e5
savan-sudhakaran <savan.sudhakaran@queberry.com>
2025-05-23 12:56:45 +0530

    Changes

---
+++ src/main/java/io/queberry/que/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+//package io.queberry.que.repository;
+//
+//
+//import com.example.QueApplication.Entity.Role;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface RoleRepository extends JpaRepository<Role,String> {
+//    Role findByName(String name);
+//}
